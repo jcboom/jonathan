f84d905846c0bacc636f64ad3abf448916151d7e
Jonathan Calzada <jonathan@users.noreply.github.com>
2017-05-11 04:48:18 -0700

    Trying height 350

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@
 		/* Added by Jonathan Calzada to cope with image not working on iphone */
 		else {
 
-			$('.js-fullheight').css('background-size', 'auto 300px');
+			$('.js-fullheight').css('background-size', 'auto 350px');
 		}
 		/* end add */
 	};
