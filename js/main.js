6dc7ea5ccec1cf87f1de26b7ce629764441b6c2a
Jonathan Calzada <jonathan@users.noreply.github.com>
2017-05-11 04:50:25 -0700

    480 on mobile

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@
 		/* Added by Jonathan Calzada to cope with image not working on iphone */
 		else {
 
-			$('.js-fullheight').css('background-size', 'auto 450px');
+			$('.js-fullheight').css('background-size', 'auto 480px');
 		}
 		/* end add */
 	};
